2e0837e2c9104d6e7e81843f64e82fc2e9aeedee
Priyanshu Mathur <mathurpriyanshu2006@gmail.com>
2025-11-23 12:41:03 +0530

    handling sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: "/mlforgex_documentation",
+    assetPrefix: "/mlforgex_documentation",
 };
 
 export default nextConfig;
